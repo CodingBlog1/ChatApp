1f3edeba5dee3e7599e3248a62e0a712520dc8e5
vipin.gupta <vipin.gupta@ongraph.com>
2023-09-15 14:28:23 +0530

    modified code

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
-from .models import Room,Message
+from .models import Room, Message
 from django.http import HttpResponse
 from django.views import View
 
-class AllRooms(View):
-    def get(self,request):
-        allroms = Room.objects.all()
-        return render(request,"rooms.html",{"rooms":allroms})
-        
-            
-class GetRoomBySlug(View):
-    def get(self,request,slug):
-        room_name=""
-        room_name=Room.objects.filter(slug=slug)
-        if room_name.exists():
-            room_name=room_name.first().name
-        messages=Message.objects.filter(room__slug__iexact=slug)
 
-        return render(request, "room.html",{"room_name":room_name,"slug":slug,'messages':messages})
-            
+class AllRooms(View):
+    def get(self, request):
+        allroms = Room.objects.all()
+        return render(request, "rooms.html", {"rooms": allroms})
+
+
+class GetRoomBySlug(View):
+    def get(self, request, slug):
+        room_name = ""
+        room_name = Room.objects.filter(slug=slug)
+        if room_name.exists():
+            room_name = room_name.first().name
+        messages = Message.objects.filter(room__slug__iexact=slug)
+
+        return render(
+            request,
+            "room.html",
+            {"room_name": room_name, "slug": slug, "messages": messages},
+        )
--- wechatpp/urls.py
+++ wechatpp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('',include('chatapp.urls')),
+    path("", include("chatapp.urls")),
 ]
